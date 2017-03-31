6715dee8b2d96d8e381bb7b099af3de0e5ae0977
Luka Jankovic <lukjan1999@gmail.com>
2017-03-31 21:01:39 +0200

    bugfixes

--- js/index.js
+++ js/index.js
@@ -6,26 +6,39 @@ $(function() {
 
         if ($("#drawer").hasClass("md-drawer__hidden")) {
             $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white is-active");
-            $("#drawer-container").attr("class", "container-drawer-shown");
             $("#drawer").attr("class", "md-drawer md-drawer__shown");
+
             $("#shade").attr("class", "shade-shown");
+
+            /*setTimeout(function() {
+                $("#shade").css("display", "inherit");
+            }, 2000);*/
 
             menuOpen = true;
             eventHandled = true;
         } else {
+
             $("#shade").attr("class", "shade-hidden");
-            $("#drawer-container").attr("class", "container-drawer-hidden");
+
             $("#drawer").attr("class", "md-drawer md-drawer__hidden");
             $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white");
+
+            /*setTimeout(function() {
+                $("#shade").css("display", "none");
+            }, 200);*/
         }
     })
 
     $(window).click(function() {
         if (menuOpen && !eventHandled) {
-            $("#drawer-container").attr("class", "container-drawer-shown");
             $("#drawer").attr("class", "md-drawer md-drawer__hidden");
             $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white");
+
             $("#shade").attr("class", "shade-hidden");
+
+            /*setTimeout(function() {
+                $("#shade").css("display", "none");
+            }, 200);*/
         }
         eventHandled = false;
     })
