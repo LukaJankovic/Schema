554ffb0208f7106c4a3102b0f42549e518fb10d1
Luka Jankovic <lukjan1999@gmail.com>
2017-03-31 14:41:01 +0200

    fixed backdrop & click to close

--- js/index.js
+++ js/index.js
@@ -6,14 +6,27 @@ $(function() {
 
         if ($("#drawer").hasClass("md-drawer__hidden")) {
             $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white is-active");
+            $("#drawer-container").attr("class", "container-drawer-shown");
             $("#drawer").attr("class", "md-drawer md-drawer__shown");
+            $("#shade").attr("class", "shade-shown");
+
             menuOpen = true;
             eventHandled = true;
-        }
-
-        else {
+        } else {
+            $("#shade").attr("class", "shade-hidden");
+            $("#drawer-container").attr("class", "container-drawer-hidden");
             $("#drawer").attr("class", "md-drawer md-drawer__hidden");
             $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white");
         }
+    })
+
+    $(window).click(function() {
+        if (menuOpen && !eventHandled) {
+            $("#drawer-container").attr("class", "container-drawer-shown");
+            $("#drawer").attr("class", "md-drawer md-drawer__hidden");
+            $("#md-drawer__trigger").attr("class", "hamburger hamburger--arrow white");
+            $("#shade").attr("class", "shade-hidden");
+        }
+        eventHandled = false;
     })
 })
